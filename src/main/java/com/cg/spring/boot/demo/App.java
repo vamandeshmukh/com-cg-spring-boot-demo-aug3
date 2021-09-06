12d68ace1dbda222114c42b8313ae7777d2bacbd
vamandeshmukh <vamandeshmukh@gmail.com>
2021-09-06 16:14:46 +0530

    Commit
    Commit

---
+++ src/main/java/com/cg/spring/boot/demo/App.java
@@ -0,0 +1,23 @@
+package com.cg.spring.boot.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+
+	private static final Logger LOG = LoggerFactory.getLogger(App.class);
+
+	public static void main(String[] args) {
+
+		LOG.info("Start...");
+
+		SpringApplication.run(App.class, args);
+
+		LOG.info("End");
+
+	}
+
+}
--- src/main/java/com/cg/spring/boot/demo/Hello.java
+++ src/main/java/com/cg/spring/boot/demo/Hello.java
@@ -1,15 +1,25 @@
 package com.cg.spring.boot.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Hello {
 
-	@GetMapping("/hello")
+	private static final Logger LOG = LoggerFactory.getLogger(Hello.class);
+
+	@GetMapping("/hello") // endpoint / API
 	public String hello() {
-		System.out.println("hello");
+		LOG.info("hello");
 		return "Hello world!";
+	}
+
+	@GetMapping("/emp")
+	public Employee emp() {
+		LOG.info("emp");
+		return new Employee(101, "Sonu", 10.5);
 	}
 
 }
