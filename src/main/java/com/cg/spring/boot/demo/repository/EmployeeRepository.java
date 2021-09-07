19dc1e5744e64961f83efc106fcb3e1f95c7798e
vamandeshmukh <vamandeshmukh@gmail.com>
2021-09-07 14:21:14 +0530

    Commit
    Commit

--- src/main/java/com/cg/spring/boot/demo/repository/EmployeeRepository.java
+++ src/main/java/com/cg/spring/boot/demo/repository/EmployeeRepository.java
@@ -1,8 +1,11 @@
 package com.cg.spring.boot.demo.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.cg.spring.boot.demo.model.Employee;
+
 @Repository
-public interface EmployeeRepository {
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
