34bc53980d015c8b163263d3f3e744d22499e29a
vamandeshmukh <vamandeshmukh@gmail.com>
2021-09-07 10:59:53 +0530

    Update Hello.java

--- src/main/java/com/cg/spring/boot/demo/Hello.java
+++ src/main/java/com/cg/spring/boot/demo/Hello.java
@@ -6,6 +6,8 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,6 +21,12 @@ public class Hello {
 		return "Hello world!";
 	}
 
+	@PostMapping("/hi")
+	public String hi(@RequestBody String myName) {
+		LOG.info("Hi " + myName);
+		return "Hi " + myName;
+	}
+
 	@GetMapping("/emp")
 	public Employee emp() {
 		Employee emp = new Employee(101, "Sonu", 10.5);
@@ -29,7 +37,7 @@ public class Hello {
 	@GetMapping("/empList")
 	public List<Employee> empList() {
 		List<Employee> empList = new ArrayList<>();
-		empList.add(new Employee(101, "Sonu", 10.5));
+		empList.add(new Employee(101, "Sonu", 10.50));
 		empList.add(new Employee(102, "Monu", 20.25));
 		empList.add(new Employee(103, "Tonu", 15.75));
 		LOG.info("empList");
