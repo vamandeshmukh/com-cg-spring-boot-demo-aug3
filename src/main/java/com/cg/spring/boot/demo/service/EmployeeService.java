9b5a2c6cc99b6d5800af4d671bb5990cdcbef4ea
vamandeshmukh <vamandeshmukh@gmail.com>
2021-09-07 14:25:07 +0530

    Update EmployeeService.java

--- src/main/java/com/cg/spring/boot/demo/service/EmployeeService.java
+++ src/main/java/com/cg/spring/boot/demo/service/EmployeeService.java
@@ -2,22 +2,29 @@ package com.cg.spring.boot.demo.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.cg.spring.boot.demo.model.Employee;
+import com.cg.spring.boot.demo.repository.EmployeeRepository;
 
 @Service
 public class EmployeeService {
+
+	@Autowired
+	EmployeeRepository employeeRepository;
 
 	private static final Logger LOG = LoggerFactory.getLogger(EmployeeService.class);
 
 	public Employee getEmployee() {
 		LOG.info("getEmployee");
-		return new Employee(101, "Sonu", 10.5);
+//		return new Employee(101, "Sonu", 10.5);
+		return employeeRepository.findById(101).get();
+
 	}
 
 	public Employee addEmployee(Employee employee) {
-		Employee emp = employee;
+		Employee emp = employeeRepository.save(employee);
 		LOG.info(emp.toString());
 		return emp;
 	}
